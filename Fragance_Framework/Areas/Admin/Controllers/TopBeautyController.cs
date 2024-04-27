23a12aeab0d76d2ecb9bfc60ede5f6272d1d2e84
Jonathan Carlos <jonathan.carllos1994@gmail.com>
2024-04-27 00:42:24 -0300

    criado a View e Post (update) da model TopBeauty

--- Fragance_Framework/Areas/Admin/Controllers/TopBeautyController.cs
+++ Fragance_Framework/Areas/Admin/Controllers/TopBeautyController.cs
@@ -17,6 +17,12 @@ namespace Fragance_Framework.Areas.Admin.Controllers
         {
             _context = context;
         }
+
+        private bool TopBeautyExists(int id)
+        {
+            return _context.TopBeauty.Any(e => e.TopBeautyId == id);
+        }
+
         public async Task<IActionResult> Index()
         {
             var lista = await _context.TopBeauty.ToListAsync();
@@ -32,13 +38,65 @@ namespace Fragance_Framework.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("TopBeautyId,CodInternoAvulso,Linha,DescProduto,UnidadeSolto,Cartucho,CaixaEmbalagem,UnidadeBlister,Cartucho12," +
             "CaixaEmbalagem144,NumProcesso,NCM,QtdCaixa,QtdCartucho,Altura,Largura,Profundidade,Peso,Altura2,Largura2,Profundidade2,Peso2," +
-            "Altura3,Largura3,Profundidade,Peso3,Altura4,Largura4,Profundidade4,Peso4")]TopBeauty topBeauty)
+            "Altura3,Largura3,Profundidade3,Peso3,Altura4,Largura4,Profundidade4,Peso4")]TopBeauty topBeauty)
         {
             if (ModelState.IsValid)
             {
                 _context.TopBeauty.Add(topBeauty);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
+            }
+            return View(topBeauty);
+        }
+
+        public async Task<IActionResult> Edit(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var editar = await _context.TopBeauty.FindAsync(Id);
+
+            if (editar == null)
+            {
+                return NotFound();
+            }
+
+            return View(editar);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, [Bind("TopBeautyId,CodInternoAvulso,Linha,DescProduto,UnidadeSolto,Cartucho,CaixaEmbalagem,UnidadeBlister,Cartucho12," +
+            "CaixaEmbalagem144,NumProcesso,NCM,QtdCaixa,QtdCartucho,Altura,Largura,Profundidade,Peso,Altura2,Largura2,Profundidade2,Peso2," +
+            "Altura3,Largura3,Profundidade3,Peso3,Altura4,Largura4,Profundidade4,Peso4")]TopBeauty topBeauty)
+        {
+            if(Id != topBeauty.TopBeautyId)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(topBeauty);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TopBeautyExists(topBeauty.TopBeautyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
             }
             return View(topBeauty);
         }
